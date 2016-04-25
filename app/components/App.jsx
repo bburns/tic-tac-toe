fd9f935d0918a847a2fd580d48481b4f22462b8c
Brian Burns <bburns.km@gmail.com>
2016-04-25 16:13:04 -0500

    draw board state

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,15 +3,19 @@ import React from 'react';
 
 
 class Board extends React.Component {
-    // constructor() {
-    //     super();
+    constructor() {
+        super();
     //     const w = window.innerWidth - 2;
     //     const h = window.innerHeight - 2;
     //     const wh = Math.min(w, h) * 0.8;
     //     var state = {};
     //     state.wh = wh;
     //     this.state = state;
-    // }
+        var state = {};
+        // state.board = [[0,0,0],[0,0,0],[0,0,0]];
+        state.board = [[0,1,0],[0,-1,0],[1,0,0]];
+        this.state = state;
+    }
     componentDidMount() {
         this.updateCanvas();
         // window.addEventListener('resize', this.handleResize.bind(this));
@@ -32,13 +36,15 @@ class Board extends React.Component {
     }
     updateCanvas() {
         
+        const gridColor = "gray";
+        
         const ctx = this.refs.canvas.getContext('2d');
         
         const w = window.innerWidth - 2;
         const h = window.innerHeight - 2;
         const wh = Math.min(w, h) * 0.8;
         const third = wh/3;
-        const sixth = third / 2;
+        const sixth = third/2;
         
         const canvas = this.refs.canvas;
         canvas.width = wh;
@@ -46,65 +52,16 @@ class Board extends React.Component {
         
         ctx.translate(0.5, 0.5); // so lines look better
         
+        // draw grid
         ctx.beginPath();
-        
-        ctx.moveTo(0, third);
-        ctx.lineTo(wh, third);
-        
-        ctx.moveTo(0, third+third);
-        ctx.lineTo(wh, third+third);
-        
-        ctx.moveTo(third, 0);
-        ctx.lineTo(third, wh);
-        
-        ctx.moveTo(third+third, 0);
-        ctx.lineTo(third+third, wh);
-        
-        ctx.strokeStyle = "gray";
+        ctx.moveTo(0, third); ctx.lineTo(wh, third);
+        ctx.moveTo(0, third+third); ctx.lineTo(wh, third+third);
+        ctx.moveTo(third, 0); ctx.lineTo(third, wh);
+        ctx.moveTo(third+third, 0); ctx.lineTo(third+third, wh);
+        ctx.strokeStyle = gridColor;
         ctx.stroke();
         
-        
-        
-        // ctx.beginPath();
-        
-        // ctx.moveTo(0, sixth);
-        // ctx.lineTo(wh, sixth);
-        
-        // ctx.moveTo(0, third + sixth);
-        // ctx.lineTo(wh, third + sixth);
-        
-        // ctx.moveTo(0, third+third + sixth);
-        // ctx.lineTo(wh, third+third + sixth);
-        
-        // ctx.moveTo(sixth, 0);
-        // ctx.lineTo(sixth, wh);
-        
-        // ctx.moveTo(third + sixth, 0);
-        // ctx.lineTo(third + sixth, wh);
-        
-        // ctx.moveTo(third+third + sixth, 0);
-        // ctx.lineTo(third+third + sixth, wh);
-        
-        // ctx.stroke();
-        
-        
-        // draw X's and O's
-        // const fontSize = third * 1.25;
-        const fontSize = third;
-        // const fontOffsetX = third * 0.0625;
-        // const fontOffsetY = third * 0.125;
-        ctx.font = fontSize + "px serif";
-        ctx.textAlign = "center";
-        ctx.textBaseline = "middle";
-        
-        
-        
-        // ctx.fillText("X",0 + fontOffsetX, third - fontOffsetY);
-        // ctx.fillText("O",third + fontOffsetX, third+third - fontOffsetY);
-        // ctx.fillText("X",sixth, sixth);
-        // ctx.fillText("O",third + sixth, third+third + sixth);
-        
-        
+        // get letter positions
         const x = [];
         const y = [];
         const yoffset = sixth / 8;
@@ -115,12 +72,29 @@ class Board extends React.Component {
         x[2] = third + third + sixth;
         y[2] = third + third + sixth + yoffset;
         
-        ctx.fillText("X",x[0], y[0]);
-        ctx.fillText("X",x[1], y[0]);
-        ctx.fillText("O",x[1], y[2]);
+        const fontSize = third;
+        ctx.font = fontSize + "px serif";
+        ctx.textAlign = "center";
+        ctx.textBaseline = "middle";
         
+        // draw X's and O's
+        // ctx.fillText("X",x[0], y[0]);
+        // ctx.fillText("X",x[1], y[0]);
+        // ctx.fillText("O",x[1], y[2]);
         
+        // draw board state
+        const board = this.state.board;
+        for (var i=0; i<3; i++) {
+            for (var j=0; j<3; j++) {
+                const piece = board[i][j];
+                if (piece!==0) {
+                    const letter = (piece > 0) ? 'X' : 'O';
+                    ctx.fillText(letter, x[j], y[i]);
+                }
+            }
+        }
     }
+    
     render() {
         return (
             <canvas id="canvas" ref="canvas" width='100%' height='100%'/>
