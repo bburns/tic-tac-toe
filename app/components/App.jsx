72e3cafcdc5c537053c5e7d1c787ab2cb0020518
Brian Burns <bburns.km@gmail.com>
2016-04-25 15:39:30 -0500

    draw rectangle and line centered on screen, handle resize events

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -3,19 +3,46 @@ import React from 'react';
 
 
 class Board extends React.Component {
+    // constructor() {
+    //     super();
+    //     const w = window.innerWidth - 2;
+    //     const h = window.innerHeight - 2;
+    //     const wh = Math.min(w, h) * 0.8;
+    //     var state = {};
+    //     state.wh = wh;
+    //     this.state = state;
+    // }
     componentDidMount() {
         this.updateCanvas();
+        // window.addEventListener('resize', this.handleResize.bind(this));
+        window.addEventListener('resize', this.updateCanvas.bind(this));
+    }
+    // handleResize() {
+    //     // this.updateCanvas();
+    //     var state = this.state;
+    //     const w = window.innerWidth - 2;
+    //     const h = window.innerHeight - 2;
+    //     const wh = Math.min(w, h) * 0.8;
+    //     state.wh = wh;
+    //     this.setState(state);
+    // }
+    componentWillUnmount() {
+        // window.removeEventListener('resize', this.handleResize.bind(this));
+        window.removeEventListener('resize', this.updateCanvas.bind(this));
     }
     updateCanvas() {
         const ctx = this.refs.canvas.getContext('2d');
         const canvas = this.refs.canvas;
-        canvas.width = window.innerWidth - 2;
-        canvas.height = window.innerHeight - 2;
-        ctx.translate(0.5, 0.5);
+        const w = window.innerWidth - 2;
+        const h = window.innerHeight - 2;
+        const wh = Math.min(w, h) * 0.8;
+        canvas.width = wh;
+        canvas.height = wh;
+        ctx.translate(0.5, 0.5); // so lines look better
         ctx.beginPath();
         ctx.moveTo(0,0);
-        ctx.lineTo(canvas.width, canvas.height);
-        ctx.rect(0,0,canvas.width, canvas.height);
+        ctx.lineTo(wh, wh);
+        ctx.rect(0,0,wh,wh);
         ctx.stroke();
     }
     render() {
